7bfcbbb4073d98310096c7d7380e9214d041a5af
Ariel Holowinski <ahw@tuta.io>
2019-05-06 09:19:36 -0300

    npm lock

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-/*
- * Hjolwynn Heroes
- * Copyright (C) 2019 Ariel Holowinski <ahw@tuta.io>
-
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- */
 import Vue from 'vue';
 import VueTippy from 'vue-tippy'
 import Vuex from 'vuex'
